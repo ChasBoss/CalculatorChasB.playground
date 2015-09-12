adc90d67aeeb07874505556be08e1252e8b1ddbb
ChasBoss <white@Whites-Mac-mini.hsd1.il.comcast.net>
2015-09-12 09:12:05 -0500

    original file
    original with no changes.

---
+++ Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var number1 = 15
+var number2 = 2
+var number3 = 3
+
+number1 = number2 + number3 // making number1 equal the sum of number2 and number3
+
+number3 / number2 // number3 divided by number 2
+number1 * number2 // number1 multiplied by number2
+number3 - number1 // number3 minus number1
+
+
+
+println(number1)
+println(number2)
+println(number3)
+
